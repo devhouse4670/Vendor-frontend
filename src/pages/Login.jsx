91de807e3b062ebe206acd42300ba3734077ae05
devhouse4670 <devhouse4670@gmail.com>
2025-12-06 10:19:57 +0400

    Update Login.jsx
    localStorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,11 +32,11 @@ const Login = () => {
     try {
       setLoading(true);
 
-      const response = await authAPI.login(formData);
+      const response = await authAPI.login(formData, { withCredentials: true });
 
       // Store token and user data
       localStorage.setItem("token", response.data.token);
-      localStorage.setItem("user", JSON.stringify(response.data.user));
+      // localStorage.setItem("user", JSON.stringify(response.data.user));
 
       // Redirect to dashboard
       navigate("/userdashboard");
